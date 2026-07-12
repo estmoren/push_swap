01525d2c37d4baafa4d13526978dcf89a6d02cbb
lumacko42 <lumacko@student.42malaga.com>
2026-07-12 22:37:21 +0200

    adding lines to push_swap.h from Ester

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lumacko <lumacko@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 12:18:26 by lumacko           #+#    #+#             */
-/*   Updated: 2026/07/09 15:28:31 by lumacko          ###   ########.fr       */
+/*   Updated: 2026/07/12 22:35:21 by lumacko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ t_stack_node	*create_stack(char **av, int ac);
 void			get_rank(t_stack_node *sa, int size);
 int				get_chunk_size(int size, int strategy);
 void			simple_sort(t_stack_node **a, t_stack_node **b);
+void			complex_sort(t_stack_node **a, t_stack_node **b);
 void			sort_chunks(t_stack_node **stack_a, t_stack_node **stack_b, int strategy);
 int				get_node_position(t_stack_node *stack, t_stack_node *target);
 int				is_bench(char *arg);
@@ -85,7 +86,7 @@ void			free_stack(t_stack_node **lst);
 void			error(t_stack_node **a, t_stack_node **b);
 void			error_parseo(void);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
-void	rotate_to_top(t_stack_node **stack_a, int top_pos, int bot_pos);
+void			rotate_to_top(t_stack_node **stack_a, int top_pos, int bot_pos);
 
 void			sa(t_stack_node **a);
 void			sb(t_stack_node **b);
